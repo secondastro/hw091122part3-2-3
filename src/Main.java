7fb9fadec75e396ffd1ab86bac0aef06a7dc97ba
User <anton_trenogin@mail.ru>
2022-11-21 12:34:25 +0500

    сделал дз по задачам и паспортам

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import Arithmetics.TaskSet;
+import Passport.DataBase;
+import Passport.Passport;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        TaskSet taskSet = new TaskSet();
+        System.out.println(taskSet);
+
+        Passport ivanov = new Passport(11223344, "Иванов", "Петр", "Степанович", "22/07/1996");
+        Passport petrov = new Passport(11335566,"Петров", "Анастас", "14/01/1965");
+        Passport sidorov = new Passport(88775566, "Сидоров", "Геннадий", "Юрьевич", "15/03/2000)");
+
+        DataBase db = new DataBase();
+        db.add(ivanov);
+        db.add(petrov);
+        db.add(sidorov);
+
+        db.add(new Passport(88775566, "Викторов", "Виктор", "Анарбекович", "14/06/1999"));
+        System.out.println(db);
+
+        System.out.println(db.find(88775566));
+        System.out.println(db.find(555555));
     }
 }
