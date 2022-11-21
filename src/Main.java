c7808d427a8f36e0f92af697f08d2f521607b1e3
User <anton_trenogin@mail.ru>
2022-11-21 17:27:41 +0500

    сделал дз по phoneBook

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import Passport.DataBase;
 import Passport.Passport;
 import PhoneBook.PhoneBook;
 
+import java.util.Map;
+
 
 public class  Main {
     public static void main(String[] args) {
@@ -25,29 +27,31 @@ public class  Main {
         System.out.println(db.find(555555));
 
         PhoneBook phoneBook = new PhoneBook();
-        phoneBook.getPhoneList().put("Сергей Бессмертный", "89999999999");
-        phoneBook.getPhoneList().put("Василий Стрельников", "89999999991");
-        phoneBook.getPhoneList().put("Игорь Ложкин", "89999999992");
-        phoneBook.getPhoneList().put("Раймонд Головачев", "89999999993");
-        phoneBook.getPhoneList().put("Ренат Магазов", "89999999994");
-        phoneBook.getPhoneList().put("Магаз Ренатов", "89999999995");
-        phoneBook.getPhoneList().put("Кудайберген Тудайберген", "89999999996");
-        phoneBook.getPhoneList().put("Алабама Василькова", "89999999997");
-        phoneBook.getPhoneList().put("Кисловодск наАмуре", "89999999998");
-        phoneBook.getPhoneList().put("Тайпси Лайтнингов", "89999999911");
-        phoneBook.getPhoneList().put("Субару Импрезов", "89999999922");
-        phoneBook.getPhoneList().put("Кассиопея Жижулина", "89999999933");
-        phoneBook.getPhoneList().put("Ставрид Карпов", "89999999944");
-        phoneBook.getPhoneList().put("Назар Балабанов", "89999999959");
-        phoneBook.getPhoneList().put("Товар Лицом", "89999999956");
-        phoneBook.getPhoneList().put("Александр Овечкин", "89999999988");
-        phoneBook.getPhoneList().put("Кети Топурия", "89999999978");
-        phoneBook.getPhoneList().put("Аскар Вафельник", "89999999945");
-        phoneBook.getPhoneList().put("Жора Крыжовников", "89999999963");
-        phoneBook.getPhoneList().put("Игорь Игорев", "89999999970");
-        phoneBook.getPhoneList().put("Анатолий Анатооолий", "89999999905");
+        phoneBook.addPhone("Сергей Бессмертный", "89999999999");
+        phoneBook.addPhone("Василий Стрельников", "89999999991");
+        phoneBook.addPhone("Игорь Ложкин", "89999999992");
+        phoneBook.addPhone("Раймонд Головачев", "89999999993");
+        phoneBook.addPhone("Ренат Магазов", "89999999994");
+        phoneBook.addPhone("Магаз Ренатов", "89999999995");
+        phoneBook.addPhone("Кудайберген Тудайберген", "89999999996");
+        phoneBook.addPhone("Алабама Василькова", "89999999997");
+        phoneBook.addPhone("Кисловодск наАмуре", "89999999998");
+        phoneBook.addPhone("Тайпси Лайтнингов", "89999999911");
+        phoneBook.addPhone("Субару Импрезов", "89999999922");
+        phoneBook.addPhone("Кассиопея Жижулина", "89999999933");
+        phoneBook.addPhone("Ставрид Карпов", "89999999944");
+        phoneBook.addPhone("Назар Балабанов", "89999999959");
+        phoneBook.addPhone("Товар Лицом", "89999999956");
+        phoneBook.addPhone("Александр Овечкин", "89999999988");
+        phoneBook.addPhone("Кети Топурия", "89999999978");
+        phoneBook.addPhone("Аскар Вафельник", "89999999945");
+        phoneBook.addPhone("Жора Крыжовников", "89999999963");
+        phoneBook.addPhone("Игорь Игорев", "89999999970");
+        phoneBook.addPhone("Анатолий Анатооолий", "89999999905");
 
-        System.out.println(phoneBook.getPhoneList().values());
+        for(Map.Entry<String,String> entry :phoneBook.getEntries()){
+            System.out.printf("%s - %s%n", entry.getKey(),entry.getValue());
+        }
     }
 
 }
--- src/PhoneBook/PhoneBook.java
+++ src/PhoneBook/PhoneBook.java
@@ -1,11 +1,17 @@
 package PhoneBook;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class PhoneBook {
     private final HashMap<String, String> phoneList = new HashMap<>();
 
-    public HashMap<String, String> getPhoneList() {
-        return phoneList;
+    public void addPhone(String name, String phone) {
+        phoneList.put(name, phone);
+    }
+
+    public Set<Map.Entry<String,String>> getEntries() {
+        return phoneList.entrySet();
     }
 }
